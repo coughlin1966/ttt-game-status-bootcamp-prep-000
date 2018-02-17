63e1af37d8f51eee39d0f8747a96f038b8387a86
Peter Coughlin <coughlin1966@gmail.com>
2018-02-17 15:49:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,8 +39,9 @@ def winner(board)
 if over?(board)
   return nil
   elsif win.all do |xwin| xwin == "X"
-    return "X wins"
+    return xwin
   else win.all do |ywin| ywin == "Y"
+    return ywin
   end
 end
 end
